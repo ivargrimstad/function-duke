7b8bdcd3bfefa5d6cd6c654f8be438250128fcc5
Ivar Grimstad <ivar.grimstad@cybercom.com>
2018-05-28 16:00:21 +0200

    Add IBM Cloud Duke

---
+++ ibm-duke/src/main/java/com/cybercom/Function.java
@@ -0,0 +1,20 @@
+package com.cybercom;
+
+import com.google.gson.JsonObject;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class Function {
+
+    public static JsonObject main(JsonObject args) {
+
+        String name = "stranger";
+        if (args.has("name"))
+            name = args.getAsJsonPrimitive("name").getAsString();
+        JsonObject response = new JsonObject();
+        response.addProperty("greeting", "Hello " + name + "!");
+
+        return response;
+    }
+}
