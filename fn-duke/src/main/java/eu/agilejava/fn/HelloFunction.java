e82be71bf2346ac10275dc504d3d63e83c105bc7
Ivar Grimstad <ivar.grimstad@cybercom.com>
2018-05-28 16:13:52 +0200

    Add Fn Duke

---
+++ fn-duke/src/main/java/eu/agilejava/fn/HelloFunction.java
@@ -0,0 +1,11 @@
+package eu.agilejava.fn;
+
+public class HelloFunction {
+
+    public String handleRequest(String input) {
+        String name = (input == null || input.isEmpty()) ? "world"  : input;
+
+        return "Hello, " + name + "!";
+    }
+
+}
