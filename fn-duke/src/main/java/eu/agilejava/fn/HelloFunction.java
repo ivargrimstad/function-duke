c022f1c25dff811755f35542c39c3cd6bb9ce5c6
Ivar Grimstad <ivar.grimstad@gmail.com>
2018-10-22 09:22:39 -0700

    Update samples

--- fn-duke/src/main/java/eu/agilejava/fn/HelloFunction.java
+++ fn-duke/src/main/java/eu/agilejava/fn/HelloFunction.java
@@ -3,9 +3,9 @@ package eu.agilejava.fn;
 public class HelloFunction {
 
     public String handleRequest(String input) {
-        String name = (input == null || input.isEmpty()) ? "world"  : input;
+        String name = (input == null || input.isEmpty()) ? "World"  : input;
 
-        return "Hello, " + name + "!";
+        return "Hello, " + name;
     }
 
 }
