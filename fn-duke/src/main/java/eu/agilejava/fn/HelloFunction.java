639340ec8cbd1eab3501bc65ed8d7b2dcc0dc828
Ivar Grimstad <ivar.grimstad@cybercom.com>
2019-01-30 13:08:17 +0100

    Add configuration of greeting

--- fn-duke/src/main/java/eu/agilejava/fn/HelloFunction.java
+++ fn-duke/src/main/java/eu/agilejava/fn/HelloFunction.java
@@ -3,7 +3,10 @@ package eu.agilejava.fn;
 public class HelloFunction {
 
     public String handleRequest(String input) {
-        String name = (input == null || input.isEmpty()) ? "World"  : input;
+
+        String place = System.getenv("place");
+
+        String name = (input == null || input.isEmpty()) ? place  : input;
 
         return "Hello, " + name + "\n";
     }
